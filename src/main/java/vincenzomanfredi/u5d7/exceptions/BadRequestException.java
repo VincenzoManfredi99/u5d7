cff88b7d86d6dafc032b93b5a86c5983cc7cd1f2
VincenzoManfredi99 <vincenzomanfredi2050@gmail.com>
2026-07-08 16:05:35 +0200

    Fix properties e badrequest

--- src/main/java/vincenzomanfredi/u5d7/exceptions/BadRequestException.java
+++ src/main/java/vincenzomanfredi/u5d7/exceptions/BadRequestException.java
@@ -1,6 +1,7 @@
 package vincenzomanfredi.u5d7.exceptions;
 
-public class BadRequestException extends RuntimeException{
+public class BadRequestException extends RuntimeException {
     public BadRequestException(String message) {
         super(message);
+    }
 }
